8aa1f550ae84127880b58fdd219aa9a1aa80c99b
Marina <m.rakovskaia@gmail.com>
2022-01-07 02:24:00 +0300

    Убрала дубликацию кода

--- src/main/java/com/example/banqueapp/AccountService.java
+++ src/main/java/com/example/banqueapp/AccountService.java
@@ -10,33 +10,30 @@ public class AccountService {
     private final HashMap<String, Integer> balanceMap = new HashMap<>();
 
     public int getBalance(String customerId) {
-        Integer balance = balanceMap.get(customerId);
-        if (balance == null) {
-            return 0;
-        }
-
+        Integer balance = getBalanceFromMap(customerId);
         return balance;
     }
 
     public void addMoney(int money, String customerId) {
-        Integer balance = balanceMap.get(customerId);
-        if (balance == null) {
-            balance = 0;
-        }
+        Integer balance = getBalanceFromMap(customerId);
         balanceMap.put(customerId, balance + money);
     }
 
     public void withdrawMoney(int money, String customerId) {
-        Integer balance = balanceMap.get(customerId);
-        if (balance == null) {
-            balance = 0;
-        }
+        Integer balance = getBalanceFromMap(customerId);
         if (balance < money){
             throw new NotEnoughMoneyException();
         }
         balanceMap.put(customerId, balance - money);
     }
 
+    private Integer getBalanceFromMap(String customerId) {
+        Integer balance = balanceMap.get(customerId);
+        if (balance == null) {
+            balance = 0;
+        }
+        return balance;
+    }
 
 
 }
